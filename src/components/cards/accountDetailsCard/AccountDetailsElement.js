a9b77fb926865e29a9daff65da95ae5e72610854
jordy_tshibss <jordytshibss@gmail.com>
2023-02-20 23:02:25 +0200

    features : board n-01 --setting-board

--- src/components/cards/accountDetailsCard/AccountDetailsElement.js
+++ src/components/cards/accountDetailsCard/AccountDetailsElement.js
@@ -6,11 +6,13 @@ export const ContentContainer = styled.div`
   gap: 30px;
   padding: 20px;
 `;
+
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
   gap: 30px;
 `;
+
 export const SpanElement = styled.span`
   color: #de821c;
 `;
