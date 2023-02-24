c32536db5fe2f0bec6651bdd41adb9d7ae4f1a33
mwingrov <mwingrove6011@gmail.com>
2023-02-24 16:51:24 +0200

    added logout and state

--- src/components/base/input/index.js
+++ src/components/base/input/index.js
@@ -1,6 +1,5 @@
 import { FormInputGroup, FormInput, FormLabel } from "./InputElement";
 
-<<<<<<< HEAD
 
 const Input = ({label ,type, onChange}) => {
 
@@ -9,14 +8,6 @@ const Input = ({label ,type, onChange}) => {
         <FormLabel>{label}</FormLabel>
         <FormInput type={type} onChange={onChange}></FormInput>      
       </FormInputGroup>
-=======
-const Input = ({ label, type }) => {
-  return (
-    <FormInputGroup>
-      <FormLabel>{label}</FormLabel>
-      <FormInput type={type}></FormInput>
-    </FormInputGroup>
->>>>>>> main
   );
 };
 
